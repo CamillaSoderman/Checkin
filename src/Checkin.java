4615ae3ec3e957e655d38973fbee371bf69140d3
CamillaSöderman <camilla.e.sodermna@gmail.com>
2022-11-09 13:24:55 +0100

    First commit, created method

--- src/Checkin.java
+++ src/Checkin.java
@@ -1,3 +1,10 @@
 public class Checkin {
 
+   String sayHello;
+
+    Checkin(String sayHello) {
+        this.sayHello = sayHello;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Check in 1 Git och Github");
+
+        Checkin hello = new Checkin("Hey");
+
+
     }
 }
